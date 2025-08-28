96bac8526a6930341e297cde31ebd775f77efb79
Pizzosta <pizzostor@gmail.com>
2025-08-28 02:25:36 +0000

    Added Forgot and reset password routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import { register, login } from '../controllers/authController.js';
+import { register, login, forgotPassword, resetPassword } from '../controllers/authController.js';
 import { validate } from '../middleware/validationMiddleware.js';
 import { authValidation } from '../utils/validators.js';
 
 const router = express.Router();
 
 // Public routes
-router.post('/register', validate(authValidation.register), register);
-router.post('/login', validate(authValidation.login), login);
+router.post('/register', validate(authValidation.register, 'body'), register);
+router.post('/login', validate(authValidation.login, 'body'), login);
+
+// Password reset routes
+router.post('/forgot-password', validate(authValidation.forgotPassword, 'body'), forgotPassword);
+router.post('/reset-password/:token', validate(authValidation.resetPassword, 'body'), resetPassword);
 
 export default router;
