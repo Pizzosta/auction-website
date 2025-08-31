3184d4c3e49ae5d79b5dcfa4f2ffb3d8b60ab705
Pizzosta <pizzostor@gmail.com>
2025-08-31 00:34:19 +0000

    Profile upload and delete added

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -2,7 +2,15 @@ import express from 'express';
 import { protect, admin } from '../middleware/authMiddleware.js';
 import { validate } from '../middleware/validationMiddleware.js';
 import { userSchema, idSchema, querySchema } from '../utils/validators.js';
-import { deleteUser, updateUser, getMe, getAllUsers } from '../controllers/userController.js';
+import { 
+    deleteUser, 
+    updateUser, 
+    getMe, 
+    getAllUsers, 
+    uploadProfilePicture,
+    deleteProfilePicture 
+} from '../controllers/userController.js';
+import { uploadProfileImageMiddleware } from '../middleware/uploadMiddleware.js';
 
 const router = express.Router();
 
@@ -13,5 +21,21 @@ router.get('/', protect, admin, validate(querySchema, 'query'), getAllUsers);
 router.get('/me', protect, getMe);
 router.delete('/:id', protect, validate(idSchema, 'params'), validate(userSchema.deleteUser, 'body'), deleteUser);
 router.patch('/:id', protect, validate(idSchema, 'params'), validate(userSchema.updateUser, 'body'), updateUser);
+
+// Profile picture routes
+router.post(
+  '/me/upload-picture',
+  protect,
+  uploadProfileImageMiddleware,
+  validate(userSchema.profilePicture, 'file'),
+  uploadProfilePicture
+);
+
+router.delete(
+  '/me/remove-picture',
+  protect,
+  validate(userSchema.deleteProfilePicture),
+  deleteProfilePicture
+);
 
 export default router;
