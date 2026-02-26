46b91a1292972d8ca6f094d5315f75c0f84f8a98
Pizzosta <pizzostor@gmail.com>
2026-02-26 17:35:21 +0000

    Implemented Spinner

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,10 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
-//import Navbar from "../components/Navbar";
-import OldFooter from "../components/OldFooter";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { useAuth } from "../context/AuthContext";
+import Spinner from "../components/Spinner";
 
 const MainLayout = () => {
   const { isLoading } = useAuth();
@@ -12,28 +11,22 @@ const MainLayout = () => {
   if (isLoading) {
     return (
       <div className="min-h-screen flex items-center justify-center">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+        <Spinner fullScreen label="Loading page..." />
       </div>
     );
   }
 
   return (
     <div className="min-h-screen flex flex-col">
-      {/*<Navbar />*/}
       <Header />
       <main className="flex-grow container mx-auto px-4 py-6">
         <Suspense
-          fallback={
-            <div className="flex justify-center items-center h-64">
-              <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
-            </div>
-          }
+         fallback={<Spinner fullScreen label="Loading page..."/>}
         >
           <Outlet />
         </Suspense>
       </main>
-      {/*<Footer />*/}
-      <OldFooter />
+      <Footer />
     </div>
   );
 };
